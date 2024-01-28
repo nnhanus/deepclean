8a99d10d6286777379957be83169480ac435d40c
Compaagnie <julien.berry@yahoo.fr>
2024-01-28 12:37:21 +0100

    move script to test

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
 
 public class move : MonoBehaviour
 {
+    [SerializeField]
+    private InputActionReference m_TriggerReference;
+    public InputActionReference triggerReference { get => m_TriggerReference; set => m_TriggerReference = value; }
+
+    [SerializeField]
+    private InputActionReference m_AxisReference;
+    public InputActionReference axisReference { get => m_AxisReference; set => m_AxisReference = value; }
+
     // Define these two as the XRRig in Unity
     public GameObject RigContainer;
     // and that as your controller
@@ -29,30 +38,37 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.primary2DAxisClick, out triggerValue) && triggerValue) {
-            InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.primary2DAxis, out fingerPress);
+        if (triggerReference != null 
+            && triggerReference.action != null 
+            && triggerReference.action.ReadValue<bool>() 
+            && axisReference != null 
+            && axisReference.action != null)
+        {
+            Vector2 value = axisReference.action.ReadValue<Vector2>();
+
             Quaternion rotation = Quaternion.Euler(xRController.gameObject.transform.rotation.eulerAngles);
             Matrix4x4 m = Matrix4x4.Rotate(rotation);
             Vector3 translateVect = new Vector3(0, 0, 0);
 
-            if (fingerPress.x < -0.5)
+            if (value.x < -0.25)
             {
                 translateVect = m.MultiplyPoint3x4(minusX);
                 //translateVect.y = 0;
             }
-            if (fingerPress.y > 0.5)
+            if (value.y > 0.25)
             {
                 translateVect = m.MultiplyPoint3x4(plusZ);
             }
-            if (fingerPress.y < -0.5)
+            if (value.y < -0.25)
             {
                 translateVect = m.MultiplyPoint3x4(minusZ);
             }
-            if (fingerPress.x > 0.5)
+            if (value.x > 0.25)
             {
                 translateVect = m.MultiplyPoint3x4(plusX);
                 //translateVect.y = 0;
             }
+            translateVect *= value;
             //translateVect.y = 0;
             // TODO: check for building colision,
             //before moving the rig, you can check if it's still in a certain area and modify the translation vector if it's trying to get out
