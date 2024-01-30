24336203aeba30c75703be6503e8507612f0b2d1
Kersten Smith <kersten.smith@u-psud.fr>
2024-01-30 11:43:58 +0100

    edit spawn size

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,12 +18,13 @@ public class Spawner : MonoBehaviour
     public float minAngle = -20;
     public float maxAngle = 20;
 
-    public float minForce = 18;
-    public float maxForce = 25;
+    public float minForce = 0;
+    public float maxForce = 1;
 
-    public float maxLifetime = 5f;
+    // public float maxLifetime = 5f;
 
     public Vector3 spawnBoundsSize;
+    private Collider collider;
 
     //private void OnEnable()
    // {
@@ -33,6 +34,8 @@ public class Spawner : MonoBehaviour
     
     private void Start()
     {
+        collider = GetComponent<Collider>();
+        spawnBoundsSize = collider.bounds.size;
          StartCoroutine(Spawn());
     }
     
@@ -44,12 +47,12 @@ public class Spawner : MonoBehaviour
 
     private IEnumerator Spawn()
     {
-
+        
         while (enabled)
         {
             Bounds spawnBounds = new Bounds(transform.position, spawnBoundsSize);
             GameObject prefab = trashPrefabs[Random.Range(0, trashPrefabs.Length)];
-
+            Debug.Log(spawnBoundsSize);
             Vector3 position = new Vector3();
             Debug.Log(spawnBounds);
             Vector3 min = spawnBounds.min;
@@ -59,16 +62,16 @@ public class Spawner : MonoBehaviour
             position.z = Random.Range(min.z, max.z);
 
             // random rotation but always towards 'up' of the parent
-            Quaternion rotation = transform.parent.rotation * Quaternion.Euler(Random.Range(minAngle, maxAngle), 0f, Random.Range(minAngle, maxAngle));
-
+            //Quaternion rotation = transform.parent.rotation * Quaternion.Euler(Random.Range(minAngle, maxAngle), 0f, Random.Range(minAngle, maxAngle));
+            Quaternion rotation = transform.parent.rotation * Quaternion.Euler(0f, 0f,0f);
 
             GameObject trash = Instantiate(prefab, position, rotation);
             trash.transform.localScale = new Vector3(0.03f, 0.03f, 0.03f);
 
-            Destroy(trash, maxLifetime);
+          //  Destroy(trash, maxLifetime);
 
-            float force = Random.Range(minForce, maxForce) / 10;
-            trash.GetComponent<Rigidbody>().AddForce(trash.transform.up * force, ForceMode.Impulse);
+            //float force = Random.Range(minForce, maxForce);
+            //trash.GetComponent<Rigidbody>().AddForce(trash.transform.up * force, ForceMode.Impulse);
 
             yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
         }
