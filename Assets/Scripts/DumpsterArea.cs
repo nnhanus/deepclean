16760b7742a16e5e1215d549344407380f020134
nnhanus <noemie.hanus@laposte.net>
2024-02-06 19:08:43 +0100

    changed dumpster sounds to trigger exit

--- Assets/Scripts/DumpsterArea.cs
+++ Assets/Scripts/DumpsterArea.cs
@@ -23,22 +23,24 @@ public class DumpsterArea : MonoBehaviour
     public void OnTriggerEnter(Collider other){
         if (other.gameObject == player){
             // Debug.Log("Trigger area");
-            float random = Random.Range(0f,1f);
             animator.SetTrigger("Open");
-            Debug.Log("random: " + random);
-            if (random < 0.33f){
-                doBetter.Play();   
-            } else if (random < 0.66f){
-                goodJob.Play();
-            }
+           
         }
-        //we should change it to quantity of trash in bag determines between the 2 sounds and then 50% chance of saying something
+        
     }
 
     public void OnTriggerExit(Collider other){
         if (other.gameObject == player){
             // Debug.Log("Trigger exit");
             // animator.SetTrigger("Close");
+             float random = Random.Range(0f,1f);
+            Debug.Log("random: " + random);
+            if (random < 0.33f){
+                doBetter.Play();   
+            } else if (random < 0.66f){
+                goodJob.Play();
+            }
+            //we should change it to quantity of trash in bag determines between the 2 sounds and then 50% chance of saying something
         }
     }
 }
