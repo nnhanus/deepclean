81bf232f5672da0613cdaee6214b73d26c6cf2af
Compaagnie <julien.berry@yahoo.fr>
2024-02-11 15:43:56 +0100

    gauge

--- Assets/Scripts/trashbag.cs
+++ Assets/Scripts/trashbag.cs
@@ -10,8 +10,8 @@ public class trashbag : MonoBehaviour
     // and that as your controller
     public GameObject xRController;
     public TextMeshPro bagDisplay;
+    public ImgsFillDynamic gaugeBar;
     private TrashPicker trashPicker;
-
     public List<GameObject> collectedTrash; //change to whatever the type for trash is
 
     // Start is called before the first frame update
@@ -24,11 +24,13 @@ public class trashbag : MonoBehaviour
     public void addTrashToBag(GameObject trash){
         collectedTrash.Add(trash);
         bagDisplay.SetText(collectedTrash.Count + "/8");
+        gaugeBar.SetValue(collectedTrash.Count / 8 * 100);
     }
 
     public void emptyTrash(){
         FindObjectOfType<GameManager>().AddToBin(collectedTrash.Count);
         collectedTrash.Clear();
         bagDisplay.SetText(collectedTrash.Count + "/8");
+        gaugeBar.SetValue(0);
     }
 }
